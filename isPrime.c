da47adf823f3154010cedaff0c82165944f9b0ea
Michael Drost <michael.drost@unb.ca>
2025-01-21 14:18:53 -0400

    question 2 and 3 assignment 1

--- isPrime.c
+++ isPrime.c
@@ -1,3 +1,18 @@
-int main(){
-    
-}
+#include <stdio.h>
+#include <stdlib.h>
+
+static int isPrime(int i){
+    int div = 1; 
+    int factors = 0; 
+    while(div < i) { 
+        if(i%div == 0){ 
+            factors++; 
+        } 
+        div++;
+    } 
+    if(factors == 1){ 
+        return 1; 
+    } 
+    return 0; 
+} 
+
---
+++ testingPrimes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "isPrime.c"
+int main(int argc, char* argv[]){
+    printf("Enter an Integer: ");
+    int n;
+    scanf("%d",&n);
+   
+    
+
+    
+    if (isPrime(n)==1){
+        printf("%d is a prime number\n",n);
+    }else{
+        printf("%d is not prime\n",n);
+    }
+    return EXIT_SUCCESS;
+}
