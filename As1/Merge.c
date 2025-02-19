4f6fbb01bb4aab2f4c7da146712bfe71396b3d3d
Brayden Lavigne <b7gy7@remotelabm20.cs.unb.ca>
2025-02-19 09:20:13 -0400

    :wq

--- As1/Merge.c
+++ As1/Merge.c
@@ -4,22 +4,25 @@
 #include<stdbool.h>
 int isFib(int i);
 int isPrime(int i);
+bool isSqr(int j);
 int main(int argc, char * * argv)
 {
 	printf("Enter a testing integer: ");
     	int x1;
     	scanf("%d",&x1);
 	printf("Is your number apart of the fibonacci seq.? \n");
-	if(isFib(x1) == 1)
+	if(isFib(x1) == 1) //RV
 	{
+	//Return Line
 		printf("YES\n");
 	}
 	else{
 		printf("NO\n");
 	}
 	printf("Is your number a prime number? \n");
-	if(isPrime(x1) == 1)
+	if(isPrime(x1) == 1) // RV
 	{
+		//Return Line
 		printf("YES\n");
 	}
 	else{
@@ -27,13 +30,14 @@ int main(int argc, char * * argv)
 	}
    	return EXIT_SUCCESS; 
 }
+
 bool isSqr(int j){
 	int x = sqrt(j);
     	return (x * x == j);
 }
 int isFib(int i){
-	bool a = isSqr((5 * (i * i) + 4))  ||  isSqr((5 * (i * i) - 4));
-	if(a == true){
+	bool a = isSqr((5 * (i * i) + 4))  ||  isSqr((5 * (i * i) - 4)); //RV
+	if(a == true){ //Return Line
 		return 1;
 	}
 	return 0;
