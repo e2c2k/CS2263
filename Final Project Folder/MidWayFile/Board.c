ef21662e100b01a6042139ff4ac2ab70415821c7
brayden-lav <brayden.lavigne@unb.ca>
2025-03-14 13:17:41 -0300

    Updated Board.c containing create, initialize and print functions

--- Final Project Folder/MidWayFile/Board.c
+++ Final Project Folder/MidWayFile/Board.c
@@ -12,18 +12,35 @@ Board *board = (Board*)malloc(sizeof(Board)); // allocate board
 }
 
 void initializeBoard(Board *board){
-  for (int i = 0; i < SIZE; i++) { //row
-        for (int j = 0; j < SIZE; j++) { //col
-            if ((i + j) % 2 == 1) { //for each odd space
-                if (i < 3) // defining which side of the board (O on the top 3 rows)
-                    board->cells[i][j] = 'O';  // Player O
-                else if (i > 4) //defining which side of the board(X on the bottom 3 rows)
+  for(int i = 0; i < SIZE; i++){ //row
+        for(int j = 0; j < SIZE; j++){ //col
+            if((i + j) % 2 == 1){ //for each odd space
+                if(i < 3){ // defining which side of the board (O on the top 3 rows)
+                    board->cells[i][j] = 'O'; // Player O
+                }
+                else if(i > 4){ //defining which side of the board(X on the bottom 3 rows)
                     board->cells[i][j] = 'X';  // Player X
-                else 
-                    board->cells[i][j] = ' ';  // Empty space
-            } else {
+                }
+            }
+            else{
+                board->cells[i][j] = ' ';  // Empty space
+            } 
+            else{
                 board->cells[i][j] = ' ';  // Empty space
             }
         }
     }
+}
+
+void printBoard(Board *board){
+    printf("\n  0 1 2 3 4 5 6 7\n"); // printing second values following format A1 for example.
+    char val = 'A';
+    for (int i = 0; i < SIZE; i++) {
+        printf("%c ", val + i); //printing first values following format A1 for example.
+        for (int j = 0; j < SIZE; j++) {
+            printf("%c ", board->cells[i][j]); // printing the X and O's on the  board
+        }
+     printf("\n");
+    }
+    printf("\n");
 }
