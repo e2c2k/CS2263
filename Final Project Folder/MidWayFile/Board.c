4172c75cc805cc8c42234bd4d98e1ec60f418a63
brayden-lav <brayden.lavigne@unb.ca>
2025-03-14 13:24:16 -0300

    Update Board.c

--- Final Project Folder/MidWayFile/Board.c
+++ Final Project Folder/MidWayFile/Board.c
@@ -4,9 +4,9 @@
 
 Board * createBoard(){
 Board *board = (Board*)malloc(sizeof(Board)); // allocate board
-    board->cells = (char**)malloc(SIZE * sizeof(char*)); // allocate array
+    board->tiles = (char**)malloc(SIZE * sizeof(char*)); // allocate array
     for (int i = 0; i < SIZE; i++) {
-        board->cells[i] = (char*)malloc(SIZE * sizeof(char)); // allocate each row
+        board->tiles[i] = (char*)malloc(SIZE * sizeof(char)); // allocate each row
     }
     return board;
 }
@@ -16,17 +16,17 @@ void initializeBoard(Board *board){
         for(int j = 0; j < SIZE; j++){ //col
             if((i + j) % 2 == 1){ //for each odd space
                 if(i < 3){ // defining which side of the board (O on the top 3 rows)
-                    board->cells[i][j] = 'O'; // Player O
+                    board->tiles[i][j] = 'O'; // Player O
                 }
                 else if(i > 4){ //defining which side of the board(X on the bottom 3 rows)
-                    board->cells[i][j] = 'X';  // Player X
+                    board->tiles[i][j] = 'X';  // Player X
                 }
             }
             else{
-                board->cells[i][j] = ' ';  // Empty space
+                board->tiles[i][j] = ' ';  // Empty space
             } 
             else{
-                board->cells[i][j] = ' ';  // Empty space
+                board->tiles[i][j] = ' ';  // Empty space
             }
         }
     }
@@ -38,7 +38,7 @@ void printBoard(Board *board){
     for (int i = 0; i < SIZE; i++) {
         printf("%c ", val + i); //printing first values following format A1 for example.
         for (int j = 0; j < SIZE; j++) {
-            printf("%c ", board->cells[i][j]); // printing the X and O's on the  board
+            printf("%c ", board->tiles[i][j]); // printing the X and O's on the  board
         }
      printf("\n");
     }
