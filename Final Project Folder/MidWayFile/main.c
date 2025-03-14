15de6fafffc4533c08ed0a08fc4de8ce53607dc7
Michael Drost <michael.drost@unb.ca>
2025-03-14 13:58:15 -0300

    updating main.c

--- Final Project Folder/MidWayFile/main.c
+++ Final Project Folder/MidWayFile/main.c
@@ -3,5 +3,37 @@
 #include <string.h>
 #define BOARD_SIZE 8
 int main(int argc, char ** argv[]){
-  
+    int choice;
+    printf("Welcome to Checkers!\n");
+
+    while (1) {
+        // Display menu
+        printf("\nMenu:\n");
+        printf("1. Start New Game\n");
+        printf("2. Display Stats \n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: // Play the Game
+                // need to find the start method																														
+                break;
+
+            case 2: // Display Stats
+                // displays the stats like wins , Another method to be made later
+                break;
+
+            case 3: // Exit
+                // A free Method to be made				
+                return 0;
+
+            default:
+                printf("Not a valid choice pick again");
+                break;
+        }
+    }
+
+    return 0; // This line is not reached due to return above
 }
+
