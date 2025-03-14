1ae432f757f8b477855558ade621ecc0f773ef86
brayden-lav <brayden.lavigne@unb.ca>
2025-03-14 12:38:08 -0300

    Created board structure -> Board.h

---
+++ Final Project Folder/MidWayFile/Board.h
@@ -0,0 +1,9 @@
+#ifndef BOARD_H
+#define BOARD_H
+#define BOARD_SIZE 8
+typedef struct{
+  char **tiles;
+}Board;
+*Board createBoard(); //to create and allocate space for array
+void printBoard(Board *board); // prints a visual board including barriers and all X and O positions
+void initializeBoard(Board *board); // creates a base board at the starting positions of checkers.
