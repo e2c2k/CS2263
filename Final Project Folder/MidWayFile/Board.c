5b4558b7fdbe498d746d1dc4f5b8947df75fb657
brayden-lav <brayden.lavigne@unb.ca>
2025-03-14 13:26:51 -0300

    Update "size" to board_size Board.c

--- Final Project Folder/MidWayFile/Board.c
+++ Final Project Folder/MidWayFile/Board.c
@@ -4,16 +4,16 @@
 
 Board * createBoard(){
 Board *board = (Board*)malloc(sizeof(Board)); // allocate board
-    board->tiles = (char**)malloc(SIZE * sizeof(char*)); // allocate array
-    for (int i = 0; i < SIZE; i++) {
-        board->tiles[i] = (char*)malloc(SIZE * sizeof(char)); // allocate each row
+    board->tiles = (char**)malloc(BOARD_SIZE * sizeof(char*)); // allocate array
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        board->tiles[i] = (char*)malloc(BOARD_SIZE * sizeof(char)); // allocate each row
     }
     return board;
 }
 
 void initializeBoard(Board *board){
-  for(int i = 0; i < SIZE; i++){ //row
-        for(int j = 0; j < SIZE; j++){ //col
+  for(int i = 0; i < BOARD_SIZE; i++){ //row
+        for(int j = 0; j < BOARD_SIZE; j++){ //col
             if((i + j) % 2 == 1){ //for each odd space
                 if(i < 3){ // defining which side of the board (O on the top 3 rows)
                     board->tiles[i][j] = 'O'; // Player O
@@ -35,9 +35,9 @@ void initializeBoard(Board *board){
 void printBoard(Board *board){
     printf("\n  0 1 2 3 4 5 6 7\n"); // printing second values following format A1 for example.
     char val = 'A';
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
         printf("%c ", val + i); //printing first values following format A1 for example.
-        for (int j = 0; j < SIZE; j++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
             printf("%c ", board->tiles[i][j]); // printing the X and O's on the  board
         }
      printf("\n");
