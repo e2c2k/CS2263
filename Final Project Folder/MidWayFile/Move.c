4602f835b519b6fb9abe0c3e3e5cbd401a148bd9
Michael <michael.drost@unb.ca>
2025-03-16 18:31:44 -0400

    added outline functions for moving pieces, still needs work

---
+++ Final Project Folder/MidWayFile/Move.c
@@ -0,0 +1,20 @@
+#include "Board.c"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Moves specified peice to the right and up
+void UpRight(Board *board){
+
+}
+//Moves piece up and left 
+void UpLeft(Board *board){
+
+}
+//Moves piece down and right
+void DownRight(Board *board){
+
+}
+//moves down and left
+void DownLeft(Board *board){
+
+}
