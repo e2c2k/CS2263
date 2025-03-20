b2bfad9306dda7020035f0428ce346566fa3baac
bread7214 <gavinhosford@gmail.com>
2025-03-20 16:18:53 -0300

    Update Move.c

--- Final Project Folder/MidWayFile/Move.c
+++ Final Project Folder/MidWayFile/Move.c
@@ -52,18 +52,29 @@ if((sc + 1 < BOARD_SIZE) && (sr + 1 < BOARD_SIZE)){
   return true;
 }
 //moves down and left
-bool DownLeft(Board *board, int sr, int sc, int *xCounter, int *oCounter){
-if((sc + 1 < BOARD_SIZE) && (sr - 1 <= 0)){
-    board->tiles[sr][sc] = board->tiles[sr -1][sc +1];
-    if(board->tiles[sr][sc] == 'x' || board->tiles[sr][sc] == 'X'){
-      xCounter--;
-      return false;
+bool DownLeft(Board *board, int sr, int sc, int *xCounter, int *oCounter, char player, char *piece){
+
+    if((sc + 1 < BOARD_SIZE) && (sr - 1 <= 0)){
+        board->tiles[sr][sc] = board->tiles[sr -1][sc +1];
+        if(jump(board, sr, sc, xCounter, oCounter, player, piece) = true){
+            board->tiles[sr][sc] = board->tiles[sr -1][sc +1];
+            if(jump(board, sr, sc, xCounter, oCounter, player, piece) = true){
+                xCounter--;
+                return false;
+            }
+        }
+        else{
+        
+        }
+    }
+}
+
+bool jump (Board *board, int sr, int sc, int *xCounter, int *oCounter, char player, char *piece){
+    if((board->tiles[sr][sc] == 'x' || board->tiles[sr][sc] == 'X') && (piece != player )){
+        return true;
     }
     if(board->tiles[sr][sc] == 'o' || board->tiles[sr][sc] == 'O'){
-      oCounter--;
-      return false;
+        return true;
     }
-  }
-  printf("Invalid move, try a differnt move.");
-  return true;
+    return false; 
 }
