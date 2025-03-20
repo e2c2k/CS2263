f532966531a49c4bb1c3f274afa8b5336185dca0
brayden-lav <brayden.lavigne@unb.ca>
2025-03-20 10:10:10 -0300

    reverted Board.h

--- Final Project Folder/MidWayFile/Board.h
+++ Final Project Folder/MidWayFile/Board.h
@@ -3,7 +3,6 @@
 #define BOARD_SIZE 8
 typedef struct{
   char **tiles; //2D array
-  int **trackBoard;
 }Board;
 Board *createBoard(); //to create and allocate space for array
 void printBoard(Board *board); // prints a visual board including barriers and all X and O positions
