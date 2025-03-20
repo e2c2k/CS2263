c49640ef56bd8b69bf2a59d43d9e96ed61d2a961
bread7214 <gavinhosford@gmail.com>
2025-03-20 16:19:36 -0300

    Update Board.c

--- Final Project Folder/MidWayFile/Board.c
+++ Final Project Folder/MidWayFile/Board.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define WHITE_BG  "\x1b[47m"  // White background
+#define BLACK_BG  "\x1b[40m"  // Black background
+#define RESET     "\x1b[0m"   // Reset colors
+
+// ANSI for checkers pieces
+#define RED_PIECE  "\x1b[31mo\x1b[0m"  // Red piece (Player O)
+#define BLACK_PIECE "\x1b[37mx\x1b[0m"  // White text on black background (Player X)
+
+
+
 Board * createBoard(){
     Board *board;
     board = (Board*)malloc(sizeof(Board)); // allocate board
@@ -33,17 +43,33 @@ void initializeBoard(Board *board){
     }
 }
 
-void printBoard(Board *board){
-    printf("\n    0     1    2    3    4    5    6    7\n"); // printing second values following format 11 for example.
-    printf("\n-------------------------------------------\n");
+void printBoard(Board *board) {
+    printf("\n    ");  
     for (int i = 0; i < BOARD_SIZE; i++) {
-        printf("%d |", i); //printing first values following format 11 for example.
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            printf("  %c |", board->tiles[i][j]); // printing the X and O's on the  board
-        }
-     printf("\n-------------------------------------------\n");
+        printf("%d   ", i); // Column numbers
     }
-    printf("\n");
+    printf("\n  ---------------------------------\n");
+
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        printf("%d |", i); // Row number
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            char *bgColor = ((i + j) % 2 == 0) ? WHITE_BG : BLACK_BG; // Alternating squares
+            
+            // Get piece character with color
+            char *piece;
+            if (board->tiles[i][j] == 'x') {
+                piece = BLACK_PIECE;  // Player X
+            } else if (board->tiles[i][j] == 'o') {
+                piece = RED_PIECE;  // Player O
+            } else {
+                piece = " ";  // Empty space
+            }
+
+            // Print with background color
+            printf("%s %s %s|", bgColor, piece, RESET);
+        }
+        printf("\n  ---------------------------------\n");
+    }
 }
 void freeBoard(Board *board){
     //GAVIN
