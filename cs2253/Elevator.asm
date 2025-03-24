36a0c5ff6d3939e6455a6207e0f69910e6e4c4c9
Michael Drost <michael.drost@unb.ca>
2025-03-24 15:11:17 -0300

    added comments elevator.asm

--- cs2253/Elevator.asm
+++ cs2253/Elevator.asm
@@ -4,33 +4,37 @@ elevatorFloor: .asciiz "\nElevator is on floor " #$s0
 personFloor: .asciiz "\nYou're on floor " #$s1
 menuChoice: .asciiz "\n1. Call Elevator\n2. Use Elevator \n3. Exit\n"
 differentFloor: .asciiz "\nError: Elevator and person are on different floors\nCall the elevator first\n"
+wrongChoice: .asciiz "\nError: Invalid choice\n"
 .text
 main:
 
     li $s0, 0 # elevator starts on floor 0
     li $s1, 0 # person starts on floor 0
 
-    jal menu
+    j menu
 
 menu:
+    # print the elevators floor
     li $v0, 4
-    la $a0, elevatorFloor
+    la $a0, elevatorFloor 
     syscall
 
     li $v0, 1
     move $a0, $s0
     syscall
 
+    # print the persons floor
     li $v0, 4
-    la $a0, personFloor
+    la $a0, personFloor 
     syscall 
 
     li $v0, 1
     move $a0, $s1
     syscall
 
+    # prints the menu for what to do next
     li $v0, 4
-    la $a0, menuChoice
+    la $a0, menuChoice 
     syscall
 
     li $v0, 5
@@ -38,13 +42,21 @@ menu:
 
     move $t0, $v0
 
+    #branches to whatever is chosen in the meny
     beq $t0, 1, callElevatorToFloor
-    beq $t0, 2, movePersonAndElevatorToFloor
+    beq $t0, 2, movePersonAndElevatorToFloor 
     beq $t0, 3, exit
+
+    # if any value other than whats showed on menu is entered 
+    # this will be printed and the menu will be called again
+    li $v0, 4
+    la $a0, wrongChoice 
+    syscall
 
     j menu
 
 callElevatorToFloor:
+    #Eneter floor to call the elevator to 
     li $v0, 4
     la $a0, prompt
     syscall
@@ -54,12 +66,11 @@ callElevatorToFloor:
 
     move $s0, $v0
 
-    jal menu
-
-    j callElevatorToFloor
+    j menu
 
 movePersonAndElevatorToFloor:
-
+    # if person and elevator are on different floors
+    # this will be printed and the menu will be called again
     bne $s0, $s1, error
 
     li $v0, 4
@@ -69,22 +80,18 @@ movePersonAndElevatorToFloor:
     li $v0, 5
     syscall
 
+    # moves the person and elevator to the same floor
     move $s0, $v0
     move $s1, $v0
 
-    jal menu
-
-    j movePersonAndElevatorToFloor
-
+    j menu
 
 error:
     li $v0, 4
     la $a0, differentFloor
     syscall
 
-    jal menu
-
-    j error
+    j menu
 
 exit:
     li $v0, 10
