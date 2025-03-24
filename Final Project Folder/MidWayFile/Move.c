e8100b5f00b498d3afe3d3f7729e57f12bc337ce
brayden-lav <brayden.lavigne@unb.ca>
2025-03-24 12:32:37 -0300

    Update Move.c

--- Final Project Folder/MidWayFile/Move.c
+++ Final Project Folder/MidWayFile/Move.c
@@ -4,13 +4,13 @@
 #include <stdbool.h>
 //sr to move row down is + 1, move up is -1
 //sc to move column right is +1, move left is -1
-bool jump(char *board, int sr, int sc, int *xCounter, int *oCounter, char player) {
+bool jump(Board *board, int sr, int sc, int *xCounter, int *oCounter, char player) {
     // Define opponent's piece
     char opponent_piece1 = (player == 'x') ? 'o' : 'x'; // Opponent’s normal piece
     char opponent_piece2 = (player == 'x') ? 'O' : 'X'; // Opponent’s king piece
 
     // Check if there is an opponent's piece at (sr, sc)
-    if (board[sr][sc] == opponent_piece1 || board[sr][sc] == opponent_piece2) {
+    if (board->tiles[sr][sc] == opponent_piece1 || board->tiles[sr][sc] == opponent_piece2) {
         // Find the landing spot
         int new_r = (player == 'o') ? sr - 1 : sr + 1; // 'o' moves up, 'x' moves down
         int new_c = (sc > 0) ? sc - 1 : sc + 1;        // Left or right
@@ -25,7 +25,7 @@ bool jump(char *board, int sr, int sc, int *xCounter, int *oCounter, char player
 }
 
 // Moves specified peice to the right and up
-bool UpRight(char *board, int sr, int sc, int *xCounter, int *oCounter, char player){ // sr = start row, sc = start collumn
+bool UpRight(Board *board, int sr, int sc, int *xCounter, int *oCounter, char player){ // sr = start row, sc = start collumn
   if ((sr - 1 >= 0) && (sc + 1 < BOARD_SIZE)) {
         int next_r = sr - 1; // Move row up
         int next_c = sc + 1; // Move left
@@ -34,10 +34,10 @@ bool UpRight(char *board, int sr, int sc, int *xCounter, int *oCounter, char pla
             int landing_r = next_r - 1; // Landing position
             int landing_c = next_c + 1;
 
-            if (landing_r >= 0 && landing_c >= 0 && board[landing_r][landing_c] == ' ') {
-                board[landing_r][landing_c] = player; // Move player to new position
-                board[next_r][next_c] = ' ';  // Remove jumped-over piece
-                board[sr][sc] = ' ';         // Clear original position
+            if (landing_r >= 0 && landing_c >= 0 && board->tiles[landing_r][landing_c] == ' ') {
+                board->tiles[landing_r][landing_c] = player; // Move player to new position
+                board->tiles[next_r][next_c] = ' ';  // Remove jumped-over piece
+                board->tiles[sr][sc] = ' ';         // Clear original position
 
                 // Decrease opponent's counter
                 if (player == 'o') (*xCounter)--;
@@ -50,7 +50,7 @@ bool UpRight(char *board, int sr, int sc, int *xCounter, int *oCounter, char pla
     return false; // No valid move
 }
 //Moves piece up and left 
-bool UpLeft(char *board, int sr, int sc, int *xCounter, int *oCounter, char player){
+bool UpLeft(Board *board, int sr, int sc, int *xCounter, int *oCounter, char player){
     if ((sr - 1 >= 0) && (sc - 1 >= 0)) {
         int next_r = sr - 1; // Move row up
         int next_c = sc - 1; // Move left
@@ -60,9 +60,9 @@ bool UpLeft(char *board, int sr, int sc, int *xCounter, int *oCounter, char play
             int landing_c = next_c - 1;
 
             if (landing_r >= 0 && landing_c >= 0 && board->tiles[landing_r][landing_c] == ' ') {
-                board[landing_r][landing_c] = player; // Move player to new position
-                board[next_r][next_c] = ' ';  // Remove jumped-over piece
-                board[sr][sc] = ' ';         // Clear original position
+                board->tiles[landing_r][landing_c] = player; // Move player to new position
+                board->tiles[next_r][next_c] = ' ';  // Remove jumped-over piece
+                board->tiles[sr][sc] = ' ';         // Clear original position
 
                 // Decrease opponent's counter
                 if (player == 'o') (*xCounter)--;
@@ -75,7 +75,7 @@ bool UpLeft(char *board, int sr, int sc, int *xCounter, int *oCounter, char play
     return false; // No valid move
 }
 //Moves piece down and right
-bool DownRight(char *board, int sr, int sc, int *xCounter, int *oCounter, char player){
+bool DownRight(Board *board, int sr, int sc, int *xCounter, int *oCounter, char player){
 
  if ((sr + 1 < BOARD_SIZE) && (sc + 1 < BOARD_SIZE)) {
         int next_r = sr + 1; // Move row down
@@ -86,9 +86,9 @@ bool DownRight(char *board, int sr, int sc, int *xCounter, int *oCounter, char p
             int landing_c = next_c + 1;
 
             if (landing_r >= 0 && landing_c >= 0 && board->tiles[landing_r][landing_c] == ' ') {
-                board[landing_r][landing_c] = player; // Move player to new position
-                board[next_r][next_c] = ' ';  // Remove jumped-over piece
-                board[sr][sc] = ' ';         // Clear original position
+                board->tiles[landing_r][landing_c] = player; // Move player to new position
+                board->tiles[next_r][next_c] = ' ';  // Remove jumped-over piece
+                board->tiles[sr][sc] = ' ';         // Clear original position
 
                 // Decrease opponent's counter
                 if (player == 'o') (*xCounter)--;
@@ -102,7 +102,7 @@ bool DownRight(char *board, int sr, int sc, int *xCounter, int *oCounter, char p
 }
 
 //moves down and left
-bool DownLeft(char *board, int sr, int sc, int *xCounter, int *oCounter, char player) {
+bool DownLeft(Board *board, int sr, int sc, int *xCounter, int *oCounter, char player) {
     // Move diagonally down-left if within bounds
     if ((sr + 1 < BOARD_SIZE) && (sc - 1 >= 0)) {
         int next_r = sr + 1; // Move row down
@@ -113,9 +113,9 @@ bool DownLeft(char *board, int sr, int sc, int *xCounter, int *oCounter, char pl
             int landing_c = next_c - 1;
 
             if (landing_r >= 0 && landing_c >= 0 && board->tiles[landing_r][landing_c] == ' ') {
-                board[landing_r][landing_c] = player; // Move player to new position
-                board[next_r][next_c] = ' ';  // Remove jumped-over piece
-                board[sr][sc] = ' ';         // Clear original position
+                board->tiles[landing_r][landing_c] = player; // Move player to new position
+                board->tiles[next_r][next_c] = ' ';  // Remove jumped-over piece
+                board->tiles[sr][sc] = ' ';         // Clear original position
 
                 // Decrease opponent's counter
                 if (player == 'o') (*xCounter)--;
