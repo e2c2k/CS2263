9f4d489eccc9b58ffed61e976ee1144478db9d99
brayden-lav <brayden.lavigne@unb.ca>
2025-03-26 22:56:59 -0300

    Update main.c Adding txt file stuff into main

--- Final_Project_Folder/MidWayFile/main.c
+++ Final_Project_Folder/MidWayFile/main.c
@@ -13,8 +13,9 @@ int main(int argc, char ** argv[]){
         // Display menu
         printf("\nMenu:\n");
         printf("1. Start New Game\n");
-        printf("2. Display Stats \n");
-        printf("3. Exit\n");
+        printf("2. Display game sstatistics \n");
+        printf("3. Reset game statistics\n");  
+        printf("4. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -33,106 +34,119 @@ int main(int argc, char ** argv[]){
                 bool valid = false;
                 while(playing){
                     printBoard(board);
+                    printf("Current Score:\nPlayer 1: %d\nPlayer 2: %d\n", 12 - xCounter, 12 - oCounter);
                     if(xCounter == 0){
                         printf(" O's win!\n");
-                        //save to document
+                        UpdateScoreBoard(2);
                         playing = false;
                     }
                     if(oCounter == 0){
                         printf("X's win!\n");
-                        //save to document
+                        UpdateScoreBoard(1);
                         playing = false;
                     }
-                    printf("player %c make your turn.\n", player);
-                    while(!valid){
-                        printf("Select the row of the piece to move: ");
-                        scanf(" %d", &sr);
-                        printf("Select the column of the piece to move: ");
-                        scanf(" %d", &sc);
-                        if(board->tiles[sr][sc] == 'x' || board->tiles[sr][sc] == 'o'){
-                            printf("\nMovement choices:\n");
-                            printf("1. Diagonal right\n");
-                            printf("2. Diagonal left\n");
-                            printf("3. Exit game\n");                            
-                            printf("Your choice: ");
-                            scanf(" %d", &pChoice);
-                            switch(pChoice){
-                                case 1:
-                                    if(board->tiles[sr][sc] == 'x'){
-                                        valid = UpRight(board->tiles, sr, sc, &xCounter, &oCounter, player);
-                                    }
-                                    else{
-                                        valid = DownRight(board->tiles, sr, sc, &xCounter, &oCounter, player);
-                                    }    
-                                    break;
-                                
-                                    case 2:
-                                        if(board->tiles[sr][sc] == 'x'){
-                                            valid = UpLeft(board->tiles, sr, sc, &xCounter, &oCounter, player);
-                                        }
-                                        else{
-                                            valid = DownLeft(board->tiles, sr, sc, &xCounter, &oCounter, player);
-                                        }
-                                    break;
-                                    case 3:
-                                        freeBoard(board); // free board so no memory leak :)
-                                        playing = false; //ends current game
-                                        
-                                    break;                                    
-                                    default:
-                                        printf("Invalid choice.\n");
-                                    break;
-                                }
-                            }
-                            else if(board->tiles[sr][sc] == 'X' || board->tiles[sr][sc] == 'O'){
+                    else{
+                        printf("player %c make your turn.\n", player);
+                        while(!valid){
+                            printf("Select the row of the piece to move: ");
+                            scanf(" %d", &sr);
+                            printf("Select the column of the piece to move: ");
+                            scanf(" %d", &sc);
+                            if(board->tiles[sr][sc] == 'x' || board->tiles[sr][sc] == 'o'){
                                 printf("\nMovement choices:\n");
-                                printf("1. Up Right\n");
-                                printf("2. Down Right\n");
-                                printf("3. Up Left\n");
-                                printf("4. Up Right\n");
+                                printf("1. Diagonal right\n");
+                                printf("2. Diagonal left\n");
+                                printf("3. Exit game\n");                            
                                 printf("Your choice: ");
                                 scanf(" %d", &pChoice);
                                 switch(pChoice){
                                     case 1:
-                                        valid = UpRight(board->tiles, sr, sc, &xCounter, &oCounter, player);
-                                    break;
+                                        if(board->tiles[sr][sc] == 'x'){
+                                            valid = UpRight(board->tiles, sr, sc, &xCounter, &oCounter, player);
+                                        }
+                                        else{
+                                            valid = DownRight(board->tiles, sr, sc, &xCounter, &oCounter, player);
+                                        }    
+                                        break;
                                     
-                                    case 2:
-                                        valid = DownRight(board->tiles, sr, sc, &xCounter, &oCounter, player); 
-                                    break;
-
-                                    case 3:
-                                            valid = UpLeft(board->tiles, sr, sc, &xCounter, &oCounter, player);
-                                    break;
-
-                                    case 4:
-                                            valid = DownLeft(board->tiles, sr, sc, &xCounter, &oCounter, player);
-                                    break;
-                                    
-                                    default:
-                                        printf("Invalid choice.\n");
-                                    break;
+                                        case 2:
+                                            if(board->tiles[sr][sc] == 'x'){
+                                                valid = UpLeft(board->tiles, sr, sc, &xCounter, &oCounter, player);
+                                            }
+                                            else{
+                                                valid = DownLeft(board->tiles, sr, sc, &xCounter, &oCounter, player);
+                                            }
+                                        break;
+                                        case 3:
+                                            freeBoard(board); // free board so no memory leak :)
+                                            playing = false; //ends current game
+                                            
+                                        break;                                    
+                                        default:
+                                            printf("Invalid choice.\n");
+                                        break;
+                                    }
                                 }
-                            }
-                            else{
-                                printf("invalid space\n");
-                            }
-                     }
-                     if(player == 'x'){
-                         player = 'o';
-                     }
-                     else{
-                         player = 'x';
-                     }
-                    valid = false;
+                                else if(board->tiles[sr][sc] == 'X' || board->tiles[sr][sc] == 'O'){
+                                    printf("\nMovement choices:\n");
+                                    printf("1. Up Right\n");
+                                    printf("2. Down Right\n");
+                                    printf("3. Up Left\n");
+                                    printf("4. Up Right\n");
+                                    printf("Your choice: ");
+                                    scanf(" %d", &pChoice);
+                                    switch(pChoice){
+                                        case 1:
+                                            valid = UpRight(board->tiles, sr, sc, &xCounter, &oCounter, player);
+                                        break;
+                                        
+                                        case 2:
+                                            valid = DownRight(board->tiles, sr, sc, &xCounter, &oCounter, player); 
+                                        break;
+    
+                                        case 3:
+                                                valid = UpLeft(board->tiles, sr, sc, &xCounter, &oCounter, player);
+                                        break;
+    
+                                        case 4:
+                                                valid = DownLeft(board->tiles, sr, sc, &xCounter, &oCounter, player);
+                                        break;
+                                        
+                                        default:
+                                            printf("Invalid choice.\n");
+                                        break;
+                                    }
+                                }
+                                else{
+                                    printf("invalid space\n");
+                                }
+                         }
+                         if(player == 'x'){
+                             player = 'o';
+                         }
+                         else{
+                             player = 'x';
+                         }
+                        valid = false;
+                    }
                 }
                 break;
 
             case 2: // Display Stats
-                // displays the stats like wins , Another method to be made later
+               char stats = GetScoreBoard();
+                printf("Current Scores: \n%s", stats);
+                freeBoard(board);
                 break;
 
-            case 3: // Exit
+            case 3: // reset statistics
+                char reset = resetScoreBoard();
+                if(reset != null){
+                    printf("%s\n", reset);
+                }
+                freeBoard(board);		
+                break;
+
+            case 4: // Exit
                 freeBoard(board);		
                 return 0;
 
